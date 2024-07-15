6763091ff8ecc88d01095d086643411aa638ed20
gnaneswara123 <gnaneswar.petla@gmail.com>
2024-07-16 00:50:11 +0530

    second  commit

--- script.js
+++ script.js
@@ -12,12 +12,23 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function showSection(sectionId) {
-        // Hide all sections
+        
         document.getElementById('redSection').style.display = 'none';
         document.getElementById('yellowSection').style.display = 'none';
         document.getElementById('greenSection').style.display = 'none';
 
-        // Show the selected section
+        
         document.getElementById(sectionId).style.display = 'block';
     }
 });
+function toggleLight(light) {
+    
+    document.querySelectorAll('.light').forEach(function(lightElement) {
+        lightElement.classList.remove('active');
+    });
+
+    
+    var lightElement = document.getElementById(light + 'Light');
+    lightElement.classList.add('active');
+}
+
